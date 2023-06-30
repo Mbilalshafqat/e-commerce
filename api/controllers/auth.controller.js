6dc01bf56312f331cbe9df3622917e0bb15c474f
Dang Khuong Nguyen <nguyenkhuong31072002@gmail.com>
2023-06-30 11:41:20 +0700

    upload

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,16 @@
 import { hashPassword } from '../helpers/auth.helper.js';
-import User from '../models/user.model.js';
+import userModel from '../models/user.model.js';
 
 export const register = async (req, res, next) => {
     try {
-        const [name, email, password, phone, address] = req.body;
+        const { name, email, password, phone, address } = req.body;
         // validation
         if (!name || !email || !password || !phone || !address) {
             return res.send({ error: 'Fields is Required' });
         }
 
         // check user
-        const existingUser = await User.findOne({ email });
+        const existingUser = await userModel.findOne({ email });
         // existing user
         if (existingUser) {
             return res.status(200).send({
@@ -21,7 +21,7 @@ export const register = async (req, res, next) => {
         // register user
         const hashedPassword = await hashPassword(password);
         // save
-        const user = new User({ name, email, phone, address, password: hashedPassword }).save();
+        const user = new userModel({ name, email, phone, address, password: hashedPassword }).save();
         res.status(201).send({
             success: true,
             message: 'User registered successfully',
